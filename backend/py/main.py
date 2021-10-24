4969f441fea260c73ac8802b8d11be5a568d2865
DriftyDev <pedro>
2021-10-24 17:47:26 +0200

    starting

--- backend/py/main.py
+++ backend/py/main.py
@@ -1,27 +1,31 @@
 import json
+import os
 
-def UpdateModInfo(Id, Name, Description, Version, McVersion, LogoFile, Url, UpdateUrl, Screenshots, Dependencies):
-    Path = "base\\src\\main\\resources\\mcmod.info"
+def UpdateModInfo(Id, Name, Description, Version, McVersion, LogoFile, Url, Screenshots, Dependencies):
+    Path =  os.path.join(os.getcwd(), "base", "src", "main", "resources", "mcmod.info")
     with open(Path, "r") as f:
-        t = f.read()
-        McModInfo = json.load(t)
-        ModId = McModInfo["modid"]
-        ModName = McModInfo["name"]
-        ModDescription = McModInfo["descripttion"]
-        ModVersion = McModInfo["version"]
-        ModMcVersion = McModInfo["mcversion"]
-        ModLogoFilePath = McModInfo["logoFile"]
-        ModUrl = McModInfo["url"]
-        ModUpdateUrl = McModInfo["updateUrl"]
-        ModScreenshots = McModInfo["screenshots"]
-        ModDependencies = McModInfo["dependencies"]
+        Raw = json.load(f)
+        McModInfo = dict(Raw)
         f.close()
-    with open(Path) as f:
-        
-        f.close()
+        with open(Path, "w") as fw:
+            McModInfo.update({"modid":f"{Id}"})
+            McModInfo.update({"name":f"{Name}"})
+            McModInfo.update({"description":f"{Description}"})
+            McModInfo.update({"version":f"{Version}"})
+            McModInfo.update({"mcversion":f"{McVersion}"})
+            McModInfo.update({"logoFile":f"{LogoFile}"})
+            McModInfo.update({"url":f"{Url}"})
+            McModInfo.update({"updateUrl":""})
+            McModInfo.update({"authorList":""})
+            McModInfo.update({"screenshots":f"{Screenshots}"})
+            McModInfo.update({"dependencies":f"{Dependencies}"})
+            json.dump(McModInfo, fw, indent = 2)
+            fw.close()
 
 def addMixin():
     with open("") as f:
         f.read()
+
+UpdateModInfo("abyss", "Abyss", "get good", "1.0", "1.12.2", "", "", "", "")        
 
 
