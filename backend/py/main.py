24fe1541c7829a51682a2c60a3d7245b25276a59
DriftyDev <pedro.lahuerta.blanco@gmail.com>
2021-10-25 19:01:48 +0200

    mcmod auto updater

--- backend/py/main.py
+++ backend/py/main.py
@@ -1,24 +1,15 @@
 import json
 import os
 
-def UpdateModInfo(Id, Name, Description, Version, McVersion, LogoFile, Url, Screenshots, Dependencies):
+def UpdateModInfo(Id, Name, Description, Version, LogoFile, Url):
     Path =  os.path.join(os.getcwd(), "base", "src", "main", "resources", "mcmod.info")
     with open(Path, "r") as f:
-        Raw = json.load(f)
-        McModInfo = dict(Raw)
+        
         f.close()
         with open(Path, "w") as fw:
-            McModInfo.update({"modid":f"{Id}"})
-            McModInfo.update({"name":f"{Name}"})
-            McModInfo.update({"description":f"{Description}"})
-            McModInfo.update({"version":f"{Version}"})
-            McModInfo.update({"mcversion":f"{McVersion}"})
-            McModInfo.update({"logoFile":f"{LogoFile}"})
-            McModInfo.update({"url":f"{Url}"})
-            McModInfo.update({"updateUrl":""})
-            McModInfo.update({"authorList":""})
-            McModInfo.update({"screenshots":f"{Screenshots}"})
-            McModInfo.update({"dependencies":f"{Dependencies}"})
+
+            McModInfo = {"modid":f"{Id}"},{"name":f"{Name}"},{"description":f"{Description}"},{"version":f"{Version}"},{"mcversion":"1.12.2"},{"logoFile":f"{LogoFile}"},{"url":f"{Url}"},{"updateUrl":""},{"authorList":[""]},{"screenshots":""},{"dependencies":""}
+
             json.dump(McModInfo, fw, indent = 2)
             fw.close()
 
@@ -26,6 +17,4 @@ def addMixin():
     with open("") as f:
         f.read()
 
-UpdateModInfo("abyss", "Abyss", "get good", "1.0", "1.12.2", "", "", "", "")        
-
-
+UpdateModInfo("abyss", "Abyss", "get good", "1.0", "", "")
