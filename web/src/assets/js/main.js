3f71125bbe3013c51b139a22c6f4e4b9b48cd104
DriftyDev <pedro.lahuerta.blanco@gmail.com>
2021-10-30 11:50:49 +0200

    js improve

--- web/src/assets/js/main.js
+++ web/src/assets/js/main.js
@@ -1,1 +1,28 @@
+var apiToken = "TEVVWF9JU19SQVRURUQ";
 
+function getDiscordId() {
+    return "";
+}
+
+function updateModInfo(modId, modName, modDescription, modVersion, modLogoFile, modUrl) {
+    var request = $.post("https://api.mcclientcreator.org/update", {
+        discordId: getDiscordId(),
+        apiToken: apiToken,
+        method: "MOD_INFO",
+        id: modId,
+        name: modName,
+        description: modDescription,
+        version: modVersion,
+        logoFile: modLogoFile,
+        url: modUrl
+    })
+}
+
+function addMixin(newMixin) {
+    var request = $.post("https://api.mcclientcreator.org/update", {
+        discordId: getDiscordId(),
+        apiToken: apiToken,
+        method: "MIXINS",
+        mixin: newMixin
+    })
+}
